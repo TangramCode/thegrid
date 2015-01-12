4df7ab43fea4f3393fbe60fcdfd4a897108624da
Bartek <bartek@tangramcare.com>
2015-01-12 11:51:38 +0100

    beginning of processing container

--- js/thegrid.js
+++ js/thegrid.js
@@ -37,11 +37,26 @@ function onGridLoad(){
 
 }
 
-function readDomNode(node){
-    if(node.classList && node.classList.contains('grid-container'))
+function readDomNode(node) {
+    if (node.classList && node.classList.contains('grid-container')) {
         alert(node.classList);
-    if(node.classList && node.classList.contains('grid-element'))
-        alert(node.classList)
+        processGridContainer(node);
+
+    }
+
+    if (node.classList && node.classList.contains('grid-element')) {
+        alert(node.classList);
+    }
+}
+
+function processGridContainer(node){
+    var cols = node.getAttribute('g-cols');
+    var rows = node.getAttribute('g-rows');
+
+    node.width = parseInt(cols) * CELL_FULL_WIDTH;
+    node.height = parseInt(rows) * CELL_FULL_HEIGHT;
+
+
 }
 
 
