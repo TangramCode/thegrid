32a4e0ea373f68e5a0c70a58b804612431fddeb1
MateuszJ <mateuszj@tangramcare.com>
2015-01-12 13:01:44 +0100

    some fixes and file rearrangement

---
+++ js/cell_functions.js
@@ -0,0 +1,14 @@
+/**
+ * Created by mj on 2015-01-12.
+ */
+
+var GRAVITY_X = "LEFT";
+var GRAVITY_Y = "TOP";
+var node;
+
+function getCellNode(node) {
+    this.node = node;
+    alert(node.classList);
+}
+
+
---
+++ js/grid_load.js
@@ -0,0 +1,41 @@
+/**
+ * Created by mj on 2015-01-12.
+ */
+
+var walkDOM = function (node,func) {
+    func(node);
+    node = node.firstChild;
+    while(node) {
+        walkDOM(node,func);
+        node = node.nextSibling;
+    }
+
+};
+
+function processGridContainer(node){
+    var cols = node.getAttribute('g-cols');
+    var rows = node.getAttribute('g-rows');
+
+    node.width = parseInt(cols) * CELL_FULL_WIDTH;
+    node.height = parseInt(rows) * CELL_FULL_HEIGHT;
+
+
+}
+
+function readDomNode(node) {
+    if (node.classList && node.classList.contains('grid-container')) {
+        alert(node.classList);
+        processGridContainer(node);
+
+    }
+
+    if (node.classList && node.classList.contains('grid-element')) {
+        getCellNode(node);
+    }
+}
+
+
+
+this.window.onload = walkDOM(document.body,function(node) {
+    readDomNode(node);
+});
