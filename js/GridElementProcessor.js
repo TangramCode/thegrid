df58396a74149ec9c2ce7da485d7377a2ed93392
MateuszJ <mateuszj@tangramcare.com>
2015-01-13 12:58:51 +0100

    finished margins

--- js/GridElementProcessor.js
+++ js/GridElementProcessor.js
@@ -4,23 +4,25 @@
 
 
 var GridElement = function(node){
-    var node = node;
     var gravity_x = "left";
     var gravity_y = "top";
     var width;
     var height;
-    var node;
     var col_x;
     var col_y;
     var margins = [0,0,0,0];
     var TOP = 0, RIGHT = 1, BOTTOM = 2, LEFT=3;
+
+    var getPixelValue = function(value) {
+        return parseInt(value.substring(0, value.length-2));
+    };
 
     var getGravity = function () {
         var gravity = node.getAttribute("g-gravity");
         var res = gravity.split("|");
         gravity_x = res[1];
         gravity_y = res[0];
-    }
+    };
 
     var getAttributes = function() {
         width = node.getAttribute("g-width");
@@ -31,10 +33,10 @@ var GridElement = function(node){
         var temp_margins = node.getAttribute("g-margins");
 
         if(temp_margins){
-            this.margins = temp_margins.split(" ");
+            margins = temp_margins.split(" ");
         }
 
-    }
+    };
 
     var calculateHorizontalGravity = function() {
         if(gravity_x==="left") {
@@ -47,7 +49,7 @@ var GridElement = function(node){
             var left = col_x * CELL_FULL_WIDTH - (width * CELL_FULL_WIDTH)/2;
             node.style.left = Math.floor(left)+"px";
         }
-    }
+    };
 
     var calculateVerticalGravity = function() {
         if(gravity_y === "top"){
@@ -61,31 +63,42 @@ var GridElement = function(node){
             top+= height * CELL_FULL_HEIGHT /2;
             node.style.top = Math.floor(top)+"px";
         }
-    }
+    };
 
     var calculateMargins = function() {
-/*
+
         if(node.style.top) {
             var margin = margins[TOP]/100 * window.innerHeight;
-            node.style.top = Math.floor(col_y * CELL_FULL_HEIGHT + margin)+"px";
+            node.style.top = Math.floor(node.offsetTop + margin)+"px";
             if(height!=="auto") {
-                node.style.height = Math.floor(height * CELL_FULL_HEIGHT - 2*margin)+"px";
+                node.style.height = Math.floor(node.offsetHeight - 2*margin)+"px";
             }
-
         }
-        if(node.style.bottom)
-        //TODO:
-            if(node.style.top)
-            //TODO:
-                if(node.style.left) {
-                    var margin = margins[LEFT]/100 * window.innerWidth;
-                    node.style.left = Math.floor(col_x * CELL_FULL_WIDTH + margin)+"px";
-                    if(width!=="auto")
-                        node.style.width = Math.floor(width * CELL_FULL_WIDTH - 2*margin)+"px";
-                }
-*/
+        if(node.style.bottom) {
+            var margin = margins[BOTTOM]/100 * window.innerHeight;
+            node.style.bottom = Math.floor(getPixelValue(node.style.bottom) + margin)+"px";
+            if(height!=="auto") {
+                node.style.height = Math.floor(node.offsetHeight - 2*margin)+"px";
+            }
+        }
 
-    }
+        if(node.style.right) {
+            var margin = margins[BOTTOM]/100 * window.innerWidth;
+            node.style.right = Math.floor(getPixelValue(node.style.right) + margin)+"px";
+            if(width!=="auto") {
+                node.style.width = Math.floor(node.offsetWidth - 2*margin)+"px";
+            }
+        }
+
+        if(node.style.left) {
+            var margin = margins[LEFT]/100 * window.innerWidth;
+            node.style.left = Math.floor(node.offsetLeft + margin)+"px";
+            if(width!=="auto")
+                node.style.width = Math.floor(node.offsetWidth - 2*margin)+"px";
+        }
+
+
+    };
 
     var  calculatePositionAndDimension = function() {
         if(width==="auto")
@@ -102,7 +115,7 @@ var GridElement = function(node){
         calculateVerticalGravity();
         calculateMargins();
 
-    }
+    };
 
 
     GridElement.prototype.process = function(){
