ffbfdef9148e7657d8cb7dfe9912b482c4fd760c
MateuszJ <mateuszj@tangramcare.com>
2015-03-10 19:43:40 +0100

    fixes for twitch

--- js/thegrid.js
+++ js/thegrid.js
@@ -5,15 +5,14 @@ var CELL_FULL_WIDTH;
 
 var CELL_FULL_HEIGHT;
 
-var HORIZONTAL_RATIO = 2.37037;
-var VERTICAL_RATIO = 0.421875;
+var CELL_FULL_HEIGHT;
 
 var VIEWPORT_WIDTH;
 var VIEWPORT_HEIGHT;
 
 var ZOOM;
 
-function onGridLoad(){
+function loadZoom() {
     var zoomFactor = 1;
     if(window.devicePixelRatio !== undefined)
         zoomFactor = window.devicePixelRatio;
@@ -21,21 +20,25 @@ function onGridLoad(){
         zoomFactor = screen.deviceXDPI / screen.logicalXDPI;
     if(zoomFactor < 1)
         zoomFactor = 1;
-
+    if(ZOOM === zoomFactor)
+        return false;
     ZOOM = zoomFactor;
-
-    VIEWPORT_WIDTH = zoomFactor * window.innerWidth;
-    VIEWPORT_HEIGHT = zoomFactor * window.innerHeight;
-
-    CELL_FULL_WIDTH = 8*zoomFactor; //vh
-    CELL_FULL_HEIGHT = 6*zoomFactor; //vw
-
-    console.log(8*zoomFactor);
+    return true;
 }
 
-onGridLoad(); //initial load
+function loadGrid() {
+    VIEWPORT_WIDTH = ZOOM * window.innerWidth;
+    VIEWPORT_HEIGHT = ZOOM * window.innerHeight;
+
+    CELL_FULL_WIDTH = 8*ZOOM; //vh
+    CELL_FULL_HEIGHT = 6*ZOOM; //vw
+}
+
+loadZoom();
+loadGrid();
 
 /**
+>>>>>>> Stashed changes
  * Created by Bartek on 2015-01-13.
  */
 
@@ -110,27 +113,27 @@ var GridElement = function(node){
 
         if(gravity_x==="left") {
             var left = col_x * CELL_FULL_WIDTH;
-            node.style.left = Math.floor(left + horizontalGravityOffset)+"vw";
+            node.style.left = left + horizontalGravityOffset+"vw";
         }  else if(gravity_x==="right") {
             var right = (node.parentNode.getAttribute("g-cols") - col_x - 1) * CELL_FULL_WIDTH;
-            node.style.right = Math.ceil(right - horizontalGravityOffset)+"vw";
+            node.style.right = right - horizontalGravityOffset+"vw";
         }else if(gravity_x === "center_horizontal"){
             var left = col_x * CELL_FULL_WIDTH - (width * CELL_FULL_WIDTH)/2;
-            node.style.left = Math.floor(left + horizontalGravityOffset)+"vw";
+            node.style.left = left + horizontalGravityOffset+"vw";
         }
     };
 
     var calculateVerticalGravity = function(verticalGravityOffset) {
         if(gravity_y === "top"){
             var top = col_y * CELL_FULL_HEIGHT;
-            node.style.top = Math.floor(top + verticalGravityOffset)+"vh";
+            node.style.top = top + verticalGravityOffset+"vh";
         }else if(gravity_y ==="bottom"){
             var bottom = (node.parentNode.getAttribute("g-rows") - col_y - 1) * CELL_FULL_HEIGHT;
-            node.style.bottom = Math.ceil(bottom - verticalGravityOffset)+"vh";
+            node.style.bottom = bottom - verticalGravityOffset+"vh";
         }else if(gravity_y === "center_vertical"){
             var top = col_y * CELL_FULL_HEIGHT;
             top+= height * CELL_FULL_HEIGHT /2;
-            node.style.top = Math.floor(top + verticalGravityOffset)+"vh";
+            node.style.top = top + verticalGravityOffset+"vh";
         }
     };
 
@@ -158,42 +161,6 @@ var GridElement = function(node){
         node.style.paddingLeft = paddingLeft+"px";
     };
 
-    var  calculatePositionAndDimension = function() {
-        var verticalGravityOffset = 0;
-        var horizontalGravityOffset = 0;
-        var newWidth;
-
-        if(width === 'match_window'){
-            
-            newWidth= VIEWPORT_WIDTH;
-        }else{
-            newWidth = width * CELL_FULL_WIDTH; 
-        }
-        
-        var newHeight = height * CELL_FULL_HEIGHT;
-
-        var dimensions = {"newWidth":newWidth, "newHeight": newHeight,
-             "horizontalGravityOffset": horizontalGravityOffset, "verticalGravityOffset":verticalGravityOffset, "widescreen":false};
-
-        if((window.innerWidth / window.innerHeight) >= 16/9)
-            dimensions.widescreen =  true;
-        else
-            dimensions.widescreen = false;
-
-        if(!is_static){
-            dimensions = calculateDynamicDimensions(dimensions);
-            dimensions = calculateDynamicSnap(dimensions);
-        }
-
-        node.style.width = Math.floor(dimensions.newWidth)+"vw";
-        node.style.height = Math.floor(dimensions.newHeight)+"vh";
-
-        calculateHorizontalGravity(dimensions.horizontalGravityOffset);
-        calculateVerticalGravity(dimensions.verticalGravityOffset);
-        calculatePaddings(dimensions.widescreen);
-
-    };
-
     var calculateDynamicDimensions = function(dimensions){
 
         if(dimensions.widescreen) {
@@ -210,26 +177,85 @@ var GridElement = function(node){
         if(snap_y === 'top') {
 
         } else if(snap_y == 'bottom') {
-            dimensions.verticalGravityOffset = Math.floor(height * CELL_FULL_HEIGHT - dimensions.newHeight);
+            dimensions.verticalGravityOffset = height * CELL_FULL_HEIGHT - dimensions.newHeight;
         } else if(snap_y == 'center'){
-            dimensions.verticalGravityOffset = Math.floor((height * CELL_FULL_HEIGHT - dimensions.newHeight)/2);
+            dimensions.verticalGravityOffset = (height * CELL_FULL_HEIGHT - dimensions.newHeight)/2;
         }
 
         if(snap_x === 'left') {
 
         } else if(snap_x == 'right') {
-            dimensions.horizontalGravityOffset = Math.floor(width * CELL_FULL_WIDTH - dimensions.newWidth);
+            dimensions.horizontalGravityOffset = width * CELL_FULL_WIDTH - dimensions.newWidth;
         } else if(snap_x == 'center'){
-            dimensions.horizontalGravityOffset = Math.floor((width * CELL_FULL_WIDTH - dimensions.newWidth)/2);
+            dimensions.horizontalGravityOffset = (width * CELL_FULL_WIDTH - dimensions.newWidth)/2;
         }
         return dimensions;
     }
 
-    GridElement.prototype.process = function(){
+    var  calculatePositionAndDimension = function () {
+        var verticalGravityOffset = 0;
+        var horizontalGravityOffset = 0;
+        var newWidth;
+
+        if(width === 'match_window'){
+
+            newWidth= VIEWPORT_WIDTH;
+        }else{
+            newWidth = width * CELL_FULL_WIDTH;
+        }
+
+        var newHeight = height * CELL_FULL_HEIGHT;
+
+        var dimensions = {"newWidth":newWidth, "newHeight": newHeight,
+             "horizontalGravityOffset": horizontalGravityOffset, "verticalGravityOffset":verticalGravityOffset, "widescreen":false};
+
+        if((window.innerWidth / window.innerHeight) >= 16/9)
+            dimensions.widescreen =  true;
+        else
+            dimensions.widescreen = false;
+
+        return dimensions;
+    }
+
+    var calculatePositionAndDimensionStatic = function() {
+        var dimensions = calculatePositionAndDimension();
+
+        node.style.width = dimensions.newWidth+"vw";
+        node.style.height = dimensions.newHeight+"vh";
+
+        calculateHorizontalGravity(dimensions.horizontalGravityOffset);
+        calculateVerticalGravity(dimensions.verticalGravityOffset);
+        calculatePaddings(dimensions.widescreen);
+
+    };
+
+    var calculatePositionAndDimensionDynamic = function() {
+
+        var dimensions = calculatePositionAndDimension();
+
+        dimensions = calculateDynamicDimensions(dimensions);
+        dimensions = calculateDynamicSnap(dimensions);
+
+        node.style.width = dimensions.newWidth+"vw";
+        node.style.height = dimensions.newHeight+"vh";
+
+        calculateHorizontalGravity(dimensions.horizontalGravityOffset);
+        calculateVerticalGravity(dimensions.verticalGravityOffset);
+        calculatePaddings(dimensions.widescreen);
+
+    };
+
+     GridElement.prototype.read = function(){
         getGravity();
         getDynamicSnap();
         getAttributes();
-        calculatePositionAndDimension();
+    };
+
+    GridElement.prototype.process = function(isStatic){
+        if(isStatic)
+            calculatePositionAndDimensionStatic();
+        else
+            calculatePositionAndDimensionDynamic();
     };
 };
 
@@ -238,26 +264,23 @@ var Grid = function() {
 
     var mouse = {x: 0, y: 0};
 
-    var readDomNode = function(node) {
-        if (node.classList && node.classList.contains('grid-container')) {
-            var container = new Container(node);
-            container.processGridContainer();
-        }
-
-        if (node.classList && node.classList.contains('grid-element')) {
+    var readDomNode = function(node, isStatic) {
             var element = new GridElement(node);
-            element.process();
-        }
+            element.read();
+            element.process(isStatic);
     };
 
-    Grid.prototype.walkDOM = function (node) {
-        readDomNode(node);
-        node = node.firstChild;
-        while(node) {
-            Grid.prototype.walkDOM(node);
-            node = node.nextSibling;
-        }
-    };
+    Grid.prototype.getGridElements = function(isStatic) {
+        var elements;
+        if(isStatic)
+            elements = document.getElementsByClassName("grid-element static");
+        else
+            elements = document.getElementsByClassName("grid-element dynamic");
+
+        Array.prototype.forEach.call(elements, function(el) {
+            readDomNode(el, isStatic);
+        });
+    }
 
     Grid.prototype.calculateFontSize = function() {
        if((window.innerWidth / window.innerHeight) >= 16/9) {
@@ -267,21 +290,37 @@ var Grid = function() {
        }
     };
 
+    Grid.prototype.calculateZoomPosition = function() {
+        var scrollX = window.innerWidth / (mouse.x / ZOOM);
+        var scrollY = window.innerHeight / (mouse.y / ZOOM);
+
+        window.scrollTo((VIEWPORT_WIDTH - (window.innerWidth )) / scrollX , ((VIEWPORT_HEIGHT - window.innerHeight) )/ scrollY );
+        mouse.x = 0;
+        mouse.y = 0 ;
+
+    };
+
     Grid.prototype.install = function(){
 
-
         window.onload = function() {
-            onGridLoad();
+            loadZoom();
+            loadGrid();
             grid.calculateFontSize();
-            grid.walkDOM(document.body);
+            grid.getGridElements(true);
+            grid.getGridElements(false);
             grid.calculateZoomPosition();
             document.getElementsByClassName("grid-container")[0].style.visibility = "visible";
         }
         window.onresize = function() {
-            onGridLoad();
-            grid.calculateFontSize();
-            grid.walkDOM(document.body);
-            grid.calculateZoomPosition();
+                var loadStatics = loadZoom();
+                loadGrid();
+                grid.calculateFontSize();
+                grid.getGridElements(false);
+                if(loadStatics) {
+                    grid.getGridElements(true);
+                    grid.calculateZoomPosition();
+                }
+
         }
 
         if (document.addEventListener) {
@@ -296,30 +335,7 @@ var Grid = function() {
             mouse.y = e.pageY;
         }
     };
-
-    Grid.prototype.calculateZoomPosition = function() {
-        var scrollX = window.innerWidth / (mouse.x / ZOOM);
-        var scrollY = window.innerHeight / (mouse.y / ZOOM);
-
-        window.scrollTo((VIEWPORT_WIDTH - (window.innerWidth )) / scrollX , ((VIEWPORT_HEIGHT - window.innerHeight) )/ scrollY );
-        mouse.x = 0;
-        mouse.y = 0 ;
-
-    };
-
-
-    
 };
 
-var Container = function(node) {
-
-    Container.prototype.processGridContainer = function() {
-        var cols = node.getAttribute('g-cols');
-        var rows = node.getAttribute('g-rows');
-
-        node.width = parseInt(cols) * CELL_FULL_WIDTH;
-        node.height = parseInt(rows) * CELL_FULL_HEIGHT;
-    };
-};
 
 
